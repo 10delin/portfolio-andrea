2e65f1fbb26a1d42a42756811cc19f22665fd85b
Antonio Delage <delincruzado_20@hotmail.com>
2023-04-26 17:27:48 +0200

    feat(header): add new model utils, add map headerItems

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import HeaderItem from "../HeaderItem/HeaderItem";
+import { HEADER_ITEMS } from "../../utils/model";
 
 const Header = () => {
   return (
     <header className="fixed w-full left-0 top-0 p-24">
       <ul className="flex flex-row w-full justify-between">
-        <HeaderItem href="/" text="Home" />
-        <HeaderItem href="/about" text="About" />
-        <HeaderItem href="/experience" text="Experience" />
-        <HeaderItem href="/contact" text="Contact" />
+        {HEADER_ITEMS.map((item) => (
+          <HeaderItem key={item.id} href={item.href} text={item.text} />
+        ))}
       </ul>
     </header>
   );
---
+++ src/utils/model.js
@@ -0,0 +1,6 @@
+export const HEADER_ITEMS = [
+  { id: 1, href: "/", text: "Home" },
+  { id: 2, href: "/about", text: "About" },
+  { id: 3, href: "/experience", text: "Experience" },
+  { id: 4, href: "/contact", text: "Contact" },
+];
